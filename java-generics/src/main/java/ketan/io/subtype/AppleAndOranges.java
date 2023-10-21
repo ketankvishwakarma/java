f3d15b99064a93fac03d9bce0fbe267cb7e671f3
Ketan Vishwakarma <ketan.vishwakarma@inventum.net>
2023-10-22 01:15:10 +0530

    added test cases

--- java-generics/src/main/java/ketan/io/subtype/AppleAndOranges.java
+++ java-generics/src/main/java/ketan/io/subtype/AppleAndOranges.java
@@ -6,29 +6,10 @@ import java.util.logging.Logger;
 /**
  * - Prohibits comparison of apples with oranges
  * - Two fruits are equal if they have the same name and the same size
-
  * */
 public class AppleAndOranges {
     protected static final Logger log= Logger.getLogger("AppleAndOranges");
 
-    public static void main(String[] args) {
-        Apple a1 = new Apple(10);
-        Apple a2 = new Apple(20);
-        Oranges o1 = new Oranges(10);
-        Oranges o2 = new Oranges(20);
-        List<Apple> a = Arrays.asList(a1,a2);
-        //assert Collections.max(a).equals(a2);
-        log.info(Collections.max(a).equals(a2)+"");
-        List<Oranges> o = Arrays.asList(o1,o2);
-        //assert Collections.max(o).equals(o2);
-        log.info(Collections.max(o).equals(o2)+"");
-
-        List<Fruit> mixed = List.of(a1,o1);
-
-        //Collections.max(mixed); // this won't compile as subtypes are different as <? extends T>
-
-
-    }
 }
 
 abstract class Fruit{
