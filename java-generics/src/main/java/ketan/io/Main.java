7f2b9b29cc75b96f1310b885980d8fc23c58114e
Ketan Vishwakarma <ketan.vishwakarma@inventum.net>
2023-10-22 01:02:32 +0530

    comparing apple and oranges

---
+++ java-generics/src/main/java/ketan/io/Main.java
@@ -0,0 +1,10 @@
+package ketan.io;
+
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger log = Logger.getLogger("org.example.main");
+    public static void main(String[] args) {
+        log.info("Hello Sweety!");
+    }
+}
